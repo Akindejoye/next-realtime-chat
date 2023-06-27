919d7f7af8ec90662164a8c0768f036286fa1894
Akindejoye <tydruhill@gmail.com>
2023-06-27 17:15:14 +0100

    first commit

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,16 @@
+import { FC } from 'react';
+import Button from '@/components/ui/Button';
+
+interface pageProps {
+  
+}
+ 
+const page: FC<pageProps> = () => {
+  return ( 
+    <div>
+      <Button>hello</Button>
+    </div>
+   );
+}
+ 
+export default page;
